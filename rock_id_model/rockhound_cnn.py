d26ce776fd26d8ce8933a5fe5d4205753c0c4958
Gecervantes01 <giovancervantes@yahoo.com>
2024-11-04 21:06:20 -0800

    Added some comments to the class and methods, also looked into ways of optimizing the models via increasing the output channels and inlcuding more convolutional layers.

--- rock_id_model/rockhound_cnn.py
+++ rock_id_model/rockhound_cnn.py
@@ -2,25 +2,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# REMINDER: look into increasing out channels and compare performance
 class RockHoundCNN(nn.Module):
+    """
+    A class defining the convolutional nueral network to be used for
+    mineral identification.
+    """
+
     def __init__(self, num_classes):
+        """
+        Defining the CNN's layers.
+        
+        The layers consist of:
+        - Convolutional
+            Detects features in the input data(images).
+        - Pooling
+            Reduces the size of the data.
+        - Flattening
+            Converts feature maps into a one-dimensional vector.
+        - Fully Connected
+            Combines previously learned features and generates
+            final output.
+        """
         super(RockHoundCNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
 
-        # Compute the flattened size after convolutions and pooling
-        self.flattened_size = 64 * 64 * 64  # Adjust based on the output size after pooling
-
-        self.fc1 = nn.Linear(self.flattened_size, 128)  # Adjust as needed
+        self.flattened_size = 64 * 64 * 64
+        
+        self.fc1 = nn.Linear(self.flattened_size, 128)
         self.fc2 = nn.Linear(128, num_classes)
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)  # Flattening while preserving batch size
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    def forward(self, inTensor):
+        """
+        Defines how the input data moves through the network upon each pass.
+
+        Parameters:
+        ----------
+        self : RockHoundCNN
+            Instance of RockHoundCNN class.
+
+        inTensor : torch.Tensor
+            The input tensor.
+        """
+        inTensor = F.relu(self.conv1(inTensor))
+        inTensor = F.max_pool2d(inTensor, 2)
+        inTensor = F.relu(self.conv2(inTensor))
+        inTensor = F.max_pool2d(inTensor, 2)
+        inTensor = torch.flatten(inTensor, 1)
+        inTensor = F.relu(self.fc1(inTensor))
+        inTensor = self.fc2(inTensor)
+        return inTensor
